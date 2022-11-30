a38f4349b9bb187d3740b9538d46112106f1610b
Daniel K. Guolo <daniel_kv@hotmail.com>
2022-11-30 10:21:20 -0300

    feat: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            white: '#ffffff',
+            black: '#000000',
+            gray: {
+                100: '#929497',
+                300: '#5B5F63',
+                500: '#24292F',
+                700: '#1B1F23',
+                900: '#121518',
+            },
+        },
     },
     plugins: [],
 }
